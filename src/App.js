62f866f7ef707f126b0c015a3bfbf617eb2ddfe1
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-06 12:46:32 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          helloooooo!
         </p>
         <a
           className="App-link"
